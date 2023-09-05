414444c107a0777c4f34469f26d64ee3c2168d4a
Pavel Danilov <Sony1000@yandex.ru>
2023-09-05 10:41:37 +0300

    fix correction after the review

--- frontend/src/utils/regularExpressions.js
+++ frontend/src/utils/regularExpressions.js
@@ -1,4 +1,4 @@
-const regExInputName = "^[0-9a-zA-ZА-Яа-яЁё\\- ]+[0-9a-zA-ZА-Яа-яЁё\\-]{2,30}$";
+const regExInputName = "^[0-9a-zA-ZА-Яа-яЁё\\- ]+[0-9a-zA-ZА-Яа-яЁё\\-]{1,30}$";
 const regExInputEmail = "[a-zA-Z0-9][a-z0-9]+\\.{1,1}[a-z]{2,3}$";
 const regExInputSearch = /^[^\s]+(\s+[^\s]+)*$/
 
